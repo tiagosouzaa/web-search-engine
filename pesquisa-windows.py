98327118765f22585455150cae798be8fafefcad
tiagosouzaa <102189534+tiagosouzaa@users.noreply.github.com>
2023-01-11 19:02:47 -0300

    Create pesquisa-windows.py

---
+++ pesquisa-windows.py
@@ -0,0 +1,10 @@
+#Faça pesquisas na web utilizando python com este simples script.
+#Versão 0.0.1 para Windows
+
+import os
+
+while True:
+    print('Pesquisar no Google ou digitar URL')
+    pesquisa = str(input())
+    print('Aguarde, Carregando... ')
+    os.startfile('https://www.google.com/search?q={}'.format(pesquisa))
